0cc09cfe8565bad88129f8c4c49d374b341206a6
Nouman Ejaz <112774247+Nouman64-cat@users.noreply.github.com>
2024-01-18 18:04:09 +0500

    Create firstFit.cpp

---
+++ firstFit.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+// Function to allocate memory to blocks as per First fit algorithm
+void firstFit(int blockSize[], int m, int processSize[], int n) {
+    // Stores block id of the block allocated to a process
+    int allocation[n];
+
+    // Initially no block is assigned to any process
+    fill_n(allocation, n, -1);
+
+    // pick each process and find the first fit block for it
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (blockSize[j] >= processSize[i]) {
+                // allocate block j to p[i] process
+                allocation[i] = j;
+
+                // Reduce available memory in this block.
+                blockSize[j] -= processSize[i];
+
+                // Break the inner loop once a fit is found
+                break;
+            }
+        }
+    }
+
+    cout << "\nProcess No.\tProcess Size\tBlock no.\n";
+    for (int i = 0; i < n; i++) {
+        cout << " " << i + 1 << "\t\t" << processSize[i] << "\t\t";
+        if (allocation[i] != -1)
+            cout << allocation[i] + 1;
+        else
+            cout << "Not Allocated";
+        cout << endl;
+    }
+}
+
+// Driver code
+int main() {
+    int blockSize[] = {100, 500, 200, 300, 600}; // Block sizes
+    int processSize[] = {212, 417, 112, 426}; // Process sizes
+    int m = sizeof(blockSize) / sizeof(blockSize[0]);
+    int n = sizeof(processSize) / sizeof(processSize[0]);
+
+    firstFit(blockSize, m, processSize, n);
+
+    return 0;
+}
