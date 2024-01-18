a708f68b1914357a884d852cfc698a6ad53b5702
Nouman Ejaz <112774247+Nouman64-cat@users.noreply.github.com>
2024-01-18 18:03:05 +0500

    Create bestFit.cpp

---
+++ bestFit.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+
+// Function to allocate memory to blocks as per Best fit algorithm
+void bestFit(int blockSize[], int m, int processSize[], int n) {
+    // Stores block id of the block allocated to a process
+    int allocation[n];
+
+    // Initially no block is assigned to any process
+    fill_n(allocation, n, -1);
+
+    // pick each process and find suitable blocks according to its size and assign to it
+    for (int i = 0; i < n; i++) {
+        int bestIdx = -1;
+        for (int j = 0; j < m; j++) {
+            if (blockSize[j] >= processSize[i]) {
+                if (bestIdx == -1)
+                    bestIdx = j;
+                else if (blockSize[bestIdx] > blockSize[j])
+                    bestIdx = j;
+            }
+        }
+
+        // If we could find a block for current process
+        if (bestIdx != -1) {
+            // allocate block j to p[i] process
+            allocation[i] = bestIdx;
+
+            // Reduce available memory in this block.
+            blockSize[bestIdx] -= processSize[i];
+        }
+    }
+
+    cout << "\nProcess No.\tProcess Size\tBlock no.\n";
+    for (int i = 0; i < n; i++) {
+        cout << " " << i+1 << "\t\t" << processSize[i] << "\t\t";
+        if (allocation[i] != -1)
+            cout << allocation[i] + 1;
+        else
+            cout << "Not Allocated";
+        cout << endl;
+    }
+}
+
+// Driver code
+int main() {
+    int blockSize[] = {100, 500, 200, 300, 600}; // Block sizes
+    int processSize[] = {212, 417, 112, 426}; // Process sizes
+    int m = sizeof(blockSize) / sizeof(blockSize[0]);
+    int n = sizeof(processSize) / sizeof(processSize[0]);
+
+    bestFit(blockSize, m, processSize, n);
+
+    return 0;
+}
